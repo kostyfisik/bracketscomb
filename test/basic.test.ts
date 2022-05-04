95986fcdfe9099ef641d41eaea8be870ec969f18
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-05-04 23:59:39 +0300

    clean up

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -26,57 +26,40 @@ function sum(str: number[]) {
   return sum
 }
 
-// https://stackoverflow.com/a/10316616/4280547
-function arraysEqual(a1: number[], a2: number[]): boolean {
-  /* WARNING: arrays must not contain {objects} or behavior may be undefined */
-  return JSON.stringify(a1) === JSON.stringify(a2)
-}
-
 // is it possible to move closing bracket (denoted by 1) to position i
 function isValidMove(str: number[], i: number) {
   const ones = sum(str.slice(0, i))
   const zeros = i - ones
-  // console.log('isValid', str,
-  //   'pos', i, str.slice(0, i),
-  //   'ones', ones,
-  //   'zeros', zeros,
-  // )
   return zeros > ones
 }
 
 function next(str: number[]) {
   let strShort = str.slice(0, -1)
   let ind0 = strShort.lastIndexOf(0)
-  // console.log('init ', str, ind0, isValidMove(str, ind0))
   if (isValidMove(str, ind0)) {
+    // Moving the right most zero
     swap(str, ind0, ind0 + 1)
-    // console.log('final', str)
     return
   }
   while (!isValidMove(str, ind0)) {
-    // console.log('in while')
+    // Searching for the fist zero we can move to the right
     strShort = str.slice(0, ind0)
     if (strShort.length === 0)
       break
-    // console.log(strShort)
     ind0 = strShort.slice(0, ind0).lastIndexOf(0)
   }
   if (ind0 === 0) {
     str.length = 0
     return
   }
-  // console.log(ind0)
   swap(str, ind0, ind0 + 1)
-  // str.slice(ind0 + 1, -1).sort()
+  // reset `str` to zeros\ones form
   const ones = sum(str.slice(ind0 + 2))
   for (let i = ind0 + 1; i < str.length; i++)
     str[i] = str.length - i > ones ? 0 : 1
-
-  // console.log('final', str, str.slice(ind0 + 2))
 }
 
-function BracketCombinations(num) {
-  const positions = num * 2
+function BracketCombinations(num: number): number {
   const initialString: number[] = []
   for (let i = 0; i < num; i++) initialString.push(0)
   for (let i = 0; i < num; i++) initialString.push(1)
@@ -91,6 +74,12 @@ function BracketCombinations(num) {
 
 // keep this function call here
 // console.log(BracketCombinations(readline()))
+
+// https://stackoverflow.com/a/10316616/4280547
+function arraysEqual(a1: number[], a2: number[]): boolean {
+  /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+  return JSON.stringify(a1) === JSON.stringify(a2)
+}
 
 describe('tests', () => {
   it('should swap', () => {
@@ -110,24 +99,15 @@ describe('tests', () => {
   })
 
   it('evaluates if swap is valid', () => {
-    //                   (())
     let arr: number[] = '0011'.split('').map(x => parseInt(x))
-    //                    ()()
     expect(isValidMove(arr, 1)).toEqual(true)
-    //                    )(()
     expect(isValidMove(arr, 0)).toEqual(false)
-    //                    )()(
     expect(isValidMove(arr, 0)).toEqual(false)
-    //     ((()))
     arr = '000111'.split('').map(x => parseInt(x))
-    //      ((()))
     expect(isValidMove(arr, 1)).toEqual(true)
-    //      ()(()))
     expect(isValidMove(arr, 1)).toEqual(true)
-    //      )((())
     expect(isValidMove(arr, 0)).toEqual(false)
     expect(isValidMove(arr, 1)).toEqual(true)
-    //     ()(())
     arr = '010011'.split('').map(x => parseInt(x))
     expect(isValidMove(arr, 1)).toEqual(true)
     expect(isValidMove(arr, 4)).toEqual(true)
@@ -158,22 +138,8 @@ describe('tests', () => {
     expect(BracketCombinations(3)).toEqual(5)
     expect(BracketCombinations(4)).toEqual(14)
     expect(BracketCombinations(5)).toEqual(42)
-    // expect(BracketCombinations(6)).toEqual(5)
     expect(BracketCombinations(7)).toEqual(429)
     expect(BracketCombinations(8)).toEqual(1430)
     expect(BracketCombinations(9)).toEqual(4862)
   })
-  // it('gets next 8 pos', () => {
-  //   const arr1: number[] = '00001111'.split('').map(x => parseInt(x))
-  //   const arr2: number[] = '00010111'.split('').map(x => parseInt(x))
-  //   const arr3: number[] = '00011011'.split('').map(x => parseInt(x))
-  //   const arr4: number[] = '00011101'.split('').map(x => parseInt(x))
-  //   const arr5: number[] = '00100111'.split('').map(x => parseInt(x))
-  //   const arr6: number[] = '00101011'.split('').map(x => parseInt(x))
-  //   const arr7: number[] = '00101101'.split('').map(x => parseInt(x))
-  //   const arr8: number[] = '00110011'.split('').map(x => parseInt(x))
-  //   const arr9: number[] = '00110101'.split('').map(x => parseInt(x))
-  //   // expect(sum(arrSum2)).toEqual(2)
-  //   // expect(sum(arrSum3)).toEqual(3)
-  // })
 })
